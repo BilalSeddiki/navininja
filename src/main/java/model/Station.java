52162472f0ae4bce3903e6c24827ef42d01c9000
e11io11 <elliottfalguerolle@gmail.com>
2023-02-24 17:50:12 +0100

    Ajout coordonnées station

--- src/main/java/model/Station.java
+++ src/main/java/model/Station.java
@@ -1,14 +1,32 @@
 package model;
 
 import java.util.ArrayList;
+import java.awt.geom.Point2D.Double;
 
 /** Une station du réseau */
 public class Station {
     /** Le nom de la station. */
     private String name;
-    /* private ??? coordinates; */
+    /** Les coordonnées gps de la station. */
+    private Double coordinates;
     /** Les differents chemins que l'on peut prendre à partir de la station. */
     private ArrayList<Path> paths;
+
+    /**
+     * Renvoie le nom de la station.
+     * @return le nom de la station
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Renvoie les coordonnées gps de la station.
+     * @return des coordonnées 2D
+     */
+    public Double getCoordinates() {
+        return coordinates;
+    }
 
     /**
      * Renvoie les differents chemins que l'on peut prendre à partir de la station.
@@ -18,11 +36,4 @@ public class Station {
         return paths;
     }
     
-    /**
-     * Renvoie le nom de la station.
-     * @return le nom de la station
-     */
-    public String getName() {
-        return name;
-    }
 }
