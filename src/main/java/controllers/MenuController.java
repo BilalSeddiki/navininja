6ec7e0b8ec956335fa20e61e941de79b8ab638f1
MARTINI reen <reen.martini@etu.u-paris.fr>
2023-03-12 15:54:59 +0100

    Ig menu

---
+++ src/main/java/controllers/MenuController.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import utils.Globals;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Controlleur de la vue Menu, se charge de définir les écouteurs des boutons présents sur l'interface.
+ * @author R. MARTINI
+ */
+public class MenuController {
+    /**
+     * Référence au composant FXML du bouton 'Network plan'
+     */
+    @FXML
+    public Button networkPlanBtn;
+    /**
+     * Référence au composant FXML du bouton 'Find a route'
+     */
+    @FXML
+    public Button findARouteBtn;
+    /**
+     * Référence au composant FXML du bouton 'Time plan'
+     */
+    @FXML
+    public Button timePlanBtn;
+
+    private Stage stage;
+    /**
+     * Constructeur
+     */
+    public MenuController() {
+    }
+
+    /**
+     * Écouteur du bouton 'Find a route'
+     * @param actionEvent événement
+     */
+    public void findARouteListener(ActionEvent actionEvent) throws IOException {
+        File file = new File(Globals.pathToView("FindARouteView.fxml"));
+        FXMLLoader loader = new FXMLLoader(file.toURI().toURL());
+        Parent root = loader.load();
+        stage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("findARoute");
+        Scene scene = new Scene(root, Globals.windowWidth(), Globals.windowHeight());
+        stage.setScene(scene);
+        stage.show();
+    }
+    /**
+     * Écouteur du bouton 'Time plan'
+     * @param actionEvent événement
+     */
+    public void timePlanListener(ActionEvent actionEvent) {
+    }
+    /**
+     * Écouteur du bouton 'Network plan'
+     * @param actionEvent événement
+     */
+    public void networkPlanListener(ActionEvent actionEvent) {
+    }
+}
