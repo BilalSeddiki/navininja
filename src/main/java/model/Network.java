7d388c3bb95a06ddba2f398bd60e09f4a137a507
e11io11 <elliottfalguerolle@gmail.com>
2023-03-16 18:21:36 +0100

    getStation avec coordonnées

--- src/main/java/model/Network.java
+++ src/main/java/model/Network.java
@@ -3,19 +3,25 @@ package model;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
+import java.awt.geom.Point2D.Double;
 
-/** Un réseau de stations */
+/** Un réseau de stations. */
 public class Network {
-    /** Ensemble de stations du réseau. <p> Une HashMap avec le nom des stations pour clés, et les stations pour valeurs.*/
-    private HashMap<String, Station> stations;
+    /** Ensemble de stations du réseau. <p> Une HashMap avec le nom des stations pour clé, et les stations pour valeur.*/
+    private HashMap<String, Station> stationsByName;
+    /** Ensemble de stations du réseau. <p> Une HashMap avec les coordonnées des stations pour clé, et les stations pour valeur.*/
+    private HashMap<Double, Station> stationsByCoordinates;
 
     /**
      * Construit un réseau de station .
      * @param stationList une liste de stations
      */
     public Network(ArrayList<Station> stationList) {
-        stations = new HashMap<String, Station>();
-        stationList.forEach(s -> stations.put(s.getName(), s));
+        stationsByName = new HashMap<String, Station>();
+        stationList.forEach(s -> {
+            stationsByName.put(s.getName(), s); 
+            stationsByCoordinates.put(s.getCoordinates(), s);
+        });
     }
 
     /**
@@ -24,7 +30,16 @@ public class Network {
      * @return true si la station existe
      */
     public boolean hasStation(String name) {
-        return stations.containsKey(name);
+        return stationsByName.containsKey(name);
+    }
+
+    /**
+     * Vérifie l'existence d'une station dans le réseau.
+     * @param name les coordonnées d'une station
+     * @return true si la station existe
+     */
+    public boolean hasStation(Double coordinates) {
+        return stationsByCoordinates.containsKey(coordinates);
     }
 
     /**
@@ -36,6 +51,18 @@ public class Network {
      * @throws NoSuchElementException si la station n'existe pas dans le réseau
      */
     public Station getStation(String name) throws NoSuchElementException {
+        return null;
+    }
+
+    /**
+     * Renvoie une station du réseau à partir d'un nom.
+     * <p>
+     * L'existence de la station dans le réseau doit être vérifié avant appel de cette fonction.
+     * @param name les coordonnées d'une station
+     * @return la station
+     * @throws NoSuchElementException si la station n'existe pas dans le réseau
+     */
+    public Station getStation(Double coordinates) throws NoSuchElementException {
         return null;
     }
 
