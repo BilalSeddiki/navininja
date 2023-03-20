129c1f3a3647e175dd16ae3eee8632d821a93190
RemedyTwo <el.famoso.remedo@gmail.com>
2023-03-20 04:23:32 +0100

    Suppression du main test

--- src/main/java/csv/ScheduleDataCsv.java
+++ src/main/java/csv/ScheduleDataCsv.java
@@ -40,31 +40,4 @@ public class ScheduleDataCsv extends CsvData<ScheduleDataCsv> {
     public String toString() {
         return line + "; " + departStation + "; " + FORMAT.format(departTime) + ";";
     }
-
-    public static void main(String[] args) {
-        String csv = 
-        "B; Borderouge; 7:00;\n" +
-        "B; Borderouge; 8:00;\n" +
-        "B; Borderouge; 9:00;\n" +
-        "B; Borderouge; 10:00;\n" +
-        "B; Borderouge; 11:00;\n" +
-        "B; Borderouge; 12:00;\n" +
-        "B; Borderouge; 13:00;\n" +
-        "B; Borderouge; 14:00;\n" +
-        "B; Borderouge; 15:00;\n" +
-        "B; Borderouge; 16:00;\n" +
-        "B; Borderouge; 17:00;\n" +
-        "B; Borderouge; 18:00;\n" +
-        "B; Borderouge; 19:00;\n" +
-        "B; Borderouge; 20:00;\n" +
-        "B; Borderouge; 21:00;\n" +
-        "B; Borderouge; 22:00;\n" +
-        "B; Borderouge; 23:00;";
-        StringReader reader = new StringReader(csv);
-        CsvToBean<ScheduleDataCsv> csvToBean = new CsvToBeanBuilder<ScheduleDataCsv>(new WhitespaceCSVReader(reader, ';')).withType(ScheduleDataCsv.class).withSeparator(';').withIgnoreEmptyLine(true).withIgnoreLeadingWhiteSpace(true).build();
-        List<ScheduleDataCsv> list = csvToBean.parse();
-        for (ScheduleDataCsv item : list) {
-            System.out.println(item);
-        }
-    }
 }
