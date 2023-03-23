429d07a9075c24bc72346741db8375479b664d09
RemedyTwo <el.famoso.remedo@gmail.com>
2023-03-23 11:11:01 +0100

    Suppression du main de test

--- src/main/java/csv/CardsDataCsv.java
+++ src/main/java/csv/CardsDataCsv.java
@@ -80,25 +80,4 @@ public class CardsDataCsv extends CsvData<CardsDataCsv> {
     public String toString() {
         return stationA + "; " + coordinatesA.get(0) + "," + coordinatesA.get(1) + "; " + stationB + "; " + coordinatesB.get(0) + "," + coordinatesB.get(1) + "; " + distance + "; " + FORMAT.format(duration) + ";";
     }
-
-    public static void main(String[] args) throws IOException {
-        List<CardsDataCsv> list = new CardsDataCsv().readCSVFile(Path.of("src/main/resources/map_data.csv"));
-        HashMap<String, Station> stations = new HashMap<>();
-        for (CardsDataCsv item : list) {
-            String stationNameA = item.getStationA();
-            double x1 = item.getCoordinatesA().get(0);
-            double y1 = item.getCoordinatesA().get(1);
-            Station stationA = new Station(stationNameA, new java.awt.geom.Point2D.Double(x1, y1));
-
-            String stationNameB = item.getStationB();
-            double x2 = item.getCoordinatesB().get(0);
-            double y2 = item.getCoordinatesB().get(1);
-            Station stationB = new Station(stationNameB, new java.awt.geom.Point2D.Double(x2, y2));
-
-            model.Path path = new model.Path(stationNameB, null, null, y2, stationA, stationB);
-
-            long time = item.getDuration().getTime();
-            System.out.println(time);
-        }
-    }
 }
