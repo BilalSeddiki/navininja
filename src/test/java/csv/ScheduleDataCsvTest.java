1ce8d0a2a4c8c2f0e62c859f78c3534ae0e24ac4
e11io11 <elliott.falguerolle@etu.u-paris.fr>
2023-04-10 19:10:51 +0200

    tests

--- src/test/java/csv/ScheduleDataCsvTest.java
+++ src/test/java/csv/ScheduleDataCsvTest.java
@@ -16,23 +16,23 @@ public class ScheduleDataCsvTest {
     @Test
     public void readCSVTest(@TempDir Path tempDir) throws IOException {
         String csv = 
-            "B;Borderouge;7:00;\n" +
-            "B;Borderouge;8:00;\n" +
-            "B;Borderouge;9:00;\n" +
-            "B;Borderouge;10:00;\n" +
-            "B;Borderouge;11:00;\n" +
-            "B;Borderouge;12:00;\n" +
-            "B;Borderouge;13:00;\n" +
-            "B;Borderouge;14:00;\n" +
-            "B;Borderouge;15:00;\n" +
-            "B;Borderouge;16:00;\n" +
-            "B;Borderouge;17:00;\n" +
-            "B;Borderouge;18:00;\n" +
-            "B;Borderouge;19:00;\n" +
-            "B;Borderouge;20:00;\n" +
-            "B;Borderouge;21:00;\n" +
-            "B;Borderouge;22:00;\n" +
-            "B;Borderouge;23:00;";
+                "B;Borderouge;7:00;1\n" +
+                        "B;Borderouge;8:00;1\n" +
+                        "B;Borderouge;9:00;1\n" +
+                        "B;Borderouge;10:00;1\n" +
+                        "B;Borderouge;11:00;1\n" +
+                        "B;Borderouge;12:00;1\n" +
+                        "B;Borderouge;13:00;1\n" +
+                        "B;Borderouge;14:00;1\n" +
+                        "B;Borderouge;15:00;1\n" +
+                        "B;Borderouge;16:00;1\n" +
+                        "B;Borderouge;17:00;1\n" +
+                        "B;Borderouge;18:00;1\n" +
+                        "B;Borderouge;19:00;1\n" +
+                        "B;Borderouge;20:00;1\n" +
+                        "B;Borderouge;21:00;1\n" +
+                        "B;Borderouge;22:00;1\n" +
+                        "B;Borderouge;23:00;1";
         Path file = tempDir.resolve("schedule_data.csv");
         Files.writeString(file, csv);
         assertTrue(Files.exists(file));
@@ -47,6 +47,7 @@ public class ScheduleDataCsvTest {
             assertEquals("Borderouge", scheduleDataCsvItem.getDepartStation());
             assertEquals(i, scheduleDataCsvItem.getDepartTime().getHour());
             assertEquals(0, scheduleDataCsvItem.getDepartTime().getMinute());
+            assertEquals("1", scheduleDataCsvItem.getVariant());
             i++;
         }
     }
--- src/test/java/model/TestNetwork.java
+++ src/test/java/model/TestNetwork.java
@@ -151,16 +151,29 @@ public class TestNetwork {
 
     @Test
     public void testFromCSV(@TempDir java.nio.file.Path tempDir) {
-        var csv = "Lourmel;2.2822419598550767, 48.83866086365992;Boucicaut;2.2879184311245595, 48.841024160993214;8 variant 1;4:14;15.93935780373747\n"
+        var csvPaths = "Lourmel;2.2822419598550767, 48.83866086365992;Boucicaut;2.2879184311245595, 48.841024160993214;8 variant 1;4:14;15.93935780373747\n"
                 +
                 "Boucicaut;2.2879184311245595, 48.841024160993214;Félix Faure;2.2918472203679703, 48.84268433479664;8 variant 1;2:58;11.195691029379345\n"
                 +
                 "Bercy;2.3791909087742877, 48.84014763512746;Gare de Lyon;2.372519782814122, 48.8442498880687;14 variant 1;7:8;26.871494140096924\n"
                 +
                 "Gare de Lyon;2.372519782814122, 48.8442498880687;Châtelet;2.346411849769497, 48.85955653272677;14 variant 1;26:45;100.92811590723446";
-        java.nio.file.Path file = tempDir.resolve("testfromcsv.csv");
+        java.nio.file.Path filePath = tempDir.resolve("testfromcsvpath.csv");
         try {
-            Files.writeString(file, csv);
+            Files.writeString(filePath, csvPaths);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        var csvSchedules = "8;Lourmel;12:30;1\n" +
+                "8;Lourmel;17:00;1\n" +
+                "8;Lourmel;18:00;1\n" +
+                "14;Bercy;12:30;1\n" +
+                "14;Bercy;17:00;1\n" +
+                "14;Bercy;18:00;1";
+        java.nio.file.Path fileSchedule = tempDir.resolve("testfromcsvschedule.csv");
+        try {
+            Files.writeString(fileSchedule, csvSchedules);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -168,24 +181,25 @@ public class TestNetwork {
         var pathList = new ArrayList<Path>();
         stationList.add(new Station("Lourmel", new Double(2.2822419598550767, 48.83866086365992)));
         stationList.add(new Station("Boucicaut", new Double(2.2879184311245595, 48.841024160993214)));
-        pathList.add(new Path("8", "1", new ArrayList<LocalTime>(), Duration.ofSeconds(60 * 4 + 14), 15.93935780373747,
+        pathList.add(new Path("8", "1",
+                new ArrayList<LocalTime>(List.of(LocalTime.of(12, 30), LocalTime.of(17, 0), LocalTime.of(18, 0))),
+                Duration.ofSeconds(60 * 4 + 14), 15.93935780373747,
                 stationList.get(0), stationList.get(1)));
         stationList.add(new Station("Félix Faure", new Double(2.2918472203679703, 48.84268433479664)));
         pathList.add(new Path("8", "1", new ArrayList<LocalTime>(), Duration.ofSeconds(2 * 60 + 58), 11.195691029379345,
                 stationList.get(1), stationList.get(2)));
         stationList.add(new Station("Bercy", new Double(2.3791909087742877, 48.84014763512746)));
         stationList.add(new Station("Gare de Lyon", new Double(2.372519782814122, 48.8442498880687)));
-        pathList.add(new Path("14", "1", new ArrayList<LocalTime>(), Duration.ofSeconds(7 * 60 + 8), 26.871494140096924,
+        pathList.add(new Path("14", "1",
+                new ArrayList<LocalTime>(List.of(LocalTime.of(12, 30), LocalTime.of(17, 0), LocalTime.of(18, 0))),
+                Duration.ofSeconds(7 * 60 + 8), 26.871494140096924,
                 stationList.get(3), stationList.get(4)));
         stationList.add(new Station("Châtelet", new Double(2.346411849769497, 48.85955653272677)));
         pathList.add(new Path("14", "1", new ArrayList<LocalTime>(), Duration.ofSeconds(26 * 60 + 45),
                 100.92811590723446, stationList.get(4), stationList.get(5)));
         Network actual;
         try {
-            actual = Network.fromCSV(file.toString(), "");
-            for (Station s : actual.getLineVariant("8", "1")) {
-                System.out.println(s);
-            }
+            actual = Network.fromCSV(filePath.toString(), fileSchedule.toString());
             var expected = new Network(stationList, pathList);
             assertEquals(expected, actual);
         } catch (IOException e) {
