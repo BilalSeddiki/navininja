9d8e80b1ddb9b482211f3e7c1adf3a0e2eff4fb1
red <red@DESKTOP-3GN2SAN>
2023-04-26 18:54:58 +0200

    jeux de message et logique du serveur pour echange schedule et best path done

---
+++ src/main/java/network/MinimalPath.java
@@ -0,0 +1,16 @@
+package netnavi;
+
+public class MinimalPath{
+    String source;
+    String destination;
+    String lineName;
+    String variant;
+    String duration;
+    public MinimalPath(String s,String d,String ln,String v,String dura){
+        source=s;
+        destination=d;
+        lineName=ln;
+        variant=v;
+        duration=dura;
+    }
+}
--- src/main/java/network/ServerMsg.java
+++ src/main/java/network/ServerMsg.java
@@ -6,6 +6,7 @@ import netnavi.NetUtil;
 import java.time.LocalTime;
 import model.*;
 import java.util.*;
+import java.time.Duration;
 
 /* la classe qui gèrent les message reseaux du server*/
 public class ServerMsg {
@@ -13,9 +14,32 @@ public class ServerMsg {
     public static void sendSchedules(Socket c, ArrayList<Schedule> s){
         String msg="";
         char size=(char)s.size();
-        msg.concat(String.valueOf(size));
+        msg=msg.concat(String.valueOf(size));
         for(Schedule a: s){
-            msg.concat(a.serial());
+            msg=msg.concat(a.serial());
+        }
+        NetUtil.send(c,msg);
+    }
+    public static void sendBestPath(Socket c, Itinerary a){
+        String msg="";
+        var tmp=a.getPaths();
+        msg=msg.concat(String.valueOf((char)tmp.size()));
+        for(var t:tmp){
+            msg=msg.concat(String.valueOf((char)t.getSource().toString().length()));
+            msg=msg.concat(t.getSource().toString());
+            msg=msg.concat(String.valueOf((char)t.getDestination().toString().length()));
+            msg=msg.concat(t.getDestination().toString());
+            msg=msg.concat(String.valueOf((char)t.getLineName().length()));
+            msg=msg.concat(t.getLineName());
+            msg=msg.concat(String.valueOf((char)t.getVariant().length()));
+            msg=msg.concat(t.getVariant());
+            Duration duration=t.getTravelDuration();
+            long hours = duration.toHours();
+            long minutes = duration.toMinutes() % 60;
+            long seconds = duration.getSeconds() % 60;
+            String d=String.format("%02d:%02d:%02d", hours, minutes, seconds);
+            msg=msg.concat(String.valueOf((char)d.length()));
+            msg=msg.concat(d);
         }
         NetUtil.send(c,msg);
     }
