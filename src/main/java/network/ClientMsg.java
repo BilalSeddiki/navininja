027058d6be11a8109e5fe3585b173a453f5198f0
red <red@DESKTOP-3GN2SAN>
2023-04-26 13:30:45 +0200

    modification pour prendre mieux en compte schedule

--- src/main/java/network/ClientMsg.java
+++ src/main/java/network/ClientMsg.java
@@ -1,13 +1,11 @@
 package netnavi;
 
-import java.net.Socket;
-import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import NetUtil;
+import java.net.*;
+import java.io.*;
+import netnavi.NetUtil;
+import java.time.LocalTime;
+import model.*;
+import java.util.*;
 
 /* la classe qui gèrent les message reseaux de l'interface */
 public class ClientMsg {
@@ -26,24 +24,26 @@ public class ClientMsg {
         NetUtil.send(s,msg);
         return receive(s);
     }*/
-    public static ArrayList<Schedule> schedule(Socket s,String station,LocalTime time){
-        String msg="hs"
+    public static ArrayList<Schedule> schedule(Socket s,String station,LocalTime time, String line){
+        String msg="hs";
         String tmp=time.toString();
-        msg.concat(station.length());
+        msg.concat(String.valueOf((char)station.length()));
         msg.concat(station);
-        msg.concat(tmp.length());
+        msg.concat(String.valueOf((char)tmp.length()));
         msg.concat(tmp);
+        msg.concat(String.valueOf((char)line.length()));
+        msg.concat(line);
         NetUtil.send(s,msg);
         String msg2=NetUtil.receive(s);
         ArrayList<Schedule> ret=new ArrayList<Schedule>();
         int size=msg2.charAt(0);
         int current0=0;
         for(int i=0;i<size;i++){
-            int i1=a.charAt(current0);
-            String d=a.substring(current0+1,current0+i1);
-            int i2=a.charAt(current0+i1);
-            String p=a.substring(current0+i1+1,current0+i1+i2);
-            current0=current0+i1+i2
+            int i1=msg2.charAt(current0);
+            String d=msg2.substring(current0+1,current0+i1+1);
+            int i2=msg2.charAt(current0+i1+1);
+            String p=msg2.substring(current0+i1+2,current0+i1+i2+1);
+            current0=current0+i1+i2+1;
             ret.add(new Schedule(d,p));
         }
 
