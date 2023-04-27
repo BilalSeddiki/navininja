3decbf1f3780a07b4dd8c1bc34adddeea0e6332b
red <red@DESKTOP-3GN2SAN>
2023-04-27 15:57:23 +0200

    correction de la doc de Minimal path qui était mal passé

--- src/main/java/netnavi/MinimalPath.java
+++ src/main/java/netnavi/MinimalPath.java
@@ -7,7 +7,12 @@ public class MinimalPath{
     public String variant;
     public String duration;
     /**
-     * un path minimal entre Deux station/deux point de coord gps
+     * Construit un Minimal Path qui a les information minimal necessaire pour l'interface graphique
+     * @param s le nom de la station de la source / ou les coordonnées GPS de la source
+     * @param d le nom de la station de destination/ ou les coordonnées GPS de destination
+     * @param ln la ligne du reseau, vaut "marche" si c'est de la marche a pied
+     * @param v le variant de la ligne, vide si ln vaut "marche"
+     * @param dura le temp de trajet du Path
      */
     public MinimalPath(String s,String d,String ln,String v,String dura){
         source=s;
