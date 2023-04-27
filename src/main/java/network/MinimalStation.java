f71963d0d49082ed1caa9419b73bdfa713a27a36
red <red@DESKTOP-3GN2SAN>
2023-04-27 14:50:03 +0200

    doc pour l'utilisation du client

--- src/main/java/network/MinimalStation.java
+++ src/main/java/network/MinimalStation.java
@@ -1,15 +1,24 @@
 package netnavi;
 import java.util.*;
 public class MinimalStation{
-    String nom;
-    ArrayList<String> lignes;
+    private String nom;
+    private ArrayList<String> lignes;
+    /**
+     * une station ayant le minimum d'information pour l'affichage
+     */
     public MinimalStation(String n, ArrayList<String> ls){
         nom=n;
         lignes=ls;
     }
+    /**
+     * renvoie le nom de la station
+     */
     public String getName(){
         return nom;
     }
+    /**
+     * renvoie la liste des lignes
+     */
     public ArrayList<String> getLignes(){
         return lignes;
     }
