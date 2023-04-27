487806461724f5315b9272abfce8f25edb2d5326
rmartini <reen.martini7@etu.univ-lorraine.fr>
2023-04-27 15:12:33 +0200

    rename package

--- src/main/java/netnavi/MinimalPath.java
+++ src/main/java/netnavi/MinimalPath.java
@@ -7,12 +7,7 @@ public class MinimalPath{
     public String variant;
     public String duration;
     /**
-     * Construit Un Path qui a les information minimal, c'est a dire les seul informations qui sont utile a l'utilisateur
-     * @param s La source du Path, peut etre une station ou des coordonnée GPS
-     * @param d La destinattion du Path, peut etre une station ou des coordonnée GPS
-     * @param ln La ligne de tranport qui est utilisée, vaut marche quand c'est de la marche a pied qui est utilisé pour le path
-     * @param v Le variant de ligne qui est utilisée, est vide quand c'est de la marche a pied qui est utilisé pour le path
-     * @param dura La durée du Path
+     * un path minimal entre Deux station/deux point de coord gps
      */
     public MinimalPath(String s,String d,String ln,String v,String dura){
         source=s;
